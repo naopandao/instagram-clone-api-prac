de956f68e5e055c536ee2739ca3475cdfd1bc517
naopandao <nao_yoshida@yunixy.co.jp>
2023-03-04 18:09:54 +0900

    create-post作成

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from sqlalchemy.sql.functions import user
 from db import models
 from db.database import engine
-from routers import user
+from routers import user, post
 
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(post.router)
 
 @app.get("/")
 def root():
