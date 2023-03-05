9d93f92e6662fd72b127f16911a93ef1b48a6a10
naopandao <nao_yoshida@yunixy.co.jp>
2023-03-05 12:44:49 +0900

    postの一覧を作成

--- routers/post.py
+++ routers/post.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from routers.schemas import PostBase, PostDisplay
 from db.database import get_db
 from db import db_post
+from typing import List
 
 router = APIRouter(
     prefix='/post',
@@ -18,3 +19,7 @@ def create(request: PostBase, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                   detail="Parameter image_url_typr can only take 'absolute' or 'relative'.")
     return db_post.create(db, request)
+
+@router.get('/all', response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
